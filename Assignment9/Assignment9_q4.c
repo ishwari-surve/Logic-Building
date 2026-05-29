8b78f2561751ef967bf2fa3892c3e3313c9f0ac0
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-05-29 12:58:49 +0530

    Create Assignment9_q4.c

---
+++ Assignment9/Assignment9_q4.c
@@ -0,0 +1,57 @@
+/*
+    Question:
+    Write a program which accept number from user
+    and return multiplication of all digits.
+
+    Input : 2395
+    Output : 270
+
+    Input : 9440
+    Output : 144
+*/
+
+#include<stdio.h>
+
+int MultDigits(int iNo)
+{
+    int iDigit = 0;
+    int iMult = 1;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+
+        if(iDigit != 0)
+        {
+            iMult = iMult * iDigit;
+        }
+
+        iNo = iNo / 10;
+    }
+
+    return iMult;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = MultDigits(iValue);
+
+    printf("Multiplication of digits is : %d",iRet);
+
+    return 0;
+}
+
+/*
+    Time Complexity : O(n)
+*/
