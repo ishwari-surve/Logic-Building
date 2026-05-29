f3c606faa92fe7271ca72a9bc498016b2aa60911
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-05-29 13:06:35 +0530

    Create Assignment11_q4.c

---
+++ Assignment11/Assignment11_q4.c
@@ -0,0 +1,61 @@
+/*
+    Question:
+    Write a program which accept range from user
+    and return addition of all even numbers in between that range.
+
+    Input : 23 30
+    Output : 108
+
+    Input : 10 18
+    Output : 70
+
+    Input : 90 18
+    Output : Invalid range
+*/
+
+#include<stdio.h>
+
+int RangeSumEven(int iStart, int iEnd)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    if((iStart < 0) || (iStart > iEnd))
+    {
+        printf("Invalid range");
+        return 0;
+    }
+
+    for(iCnt = iStart; iCnt <= iEnd; iCnt++)
+    {
+        if((iCnt % 2) == 0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+
+    return iSum;
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+    int iRet = 0;
+
+    printf("Enter starting point : ");
+    scanf("%d",&iValue1);
+
+    printf("Enter ending point : ");
+    scanf("%d",&iValue2);
+
+    iRet = RangeSumEven(iValue1, iValue2);
+
+    printf("Addition is : %d",iRet);
+
+    return 0;
+}
+
+/*
+    Time Complexity : O(n)
+*/
