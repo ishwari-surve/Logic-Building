9613726273d8d9eec7d7729c0c6edeacb0fa750b
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-05-29 13:09:28 +0530

    Create Assignment12_q2.c

---
+++ Assignment12/Assignment12_q2.c
@@ -0,0 +1,68 @@
+/*
+    Question:
+    Write a program which accept number from user
+    and check whether it contains 0 in it or not.
+
+    Input : 2395
+    Output : There is no Zero
+
+    Input : 1018
+    Output : It Contains Zero
+*/
+
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkZero(int iNo)
+{
+    int iDigit = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+
+        if(iDigit == 0)
+        {
+            return TRUE;
+        }
+
+        iNo = iNo / 10;
+    }
+
+    return FALSE;
+}
+
+int main()
+{
+    int iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    bRet = ChkZero(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It Contains Zero");
+    }
+    else
+    {
+        printf("There is no Zero");
+    }
+
+    return 0;
+}
+
+/*
+    Time Complexity : O(n)
+*/
