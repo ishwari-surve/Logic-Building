6d6f069969ba7d9d3e2e68187a27eca5d7cc7655
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-05-29 13:34:54 +0530

    Create Assignment12_q4.c

---
+++ Assignment12/Assignment12_q4.c
@@ -0,0 +1,57 @@
+/*
+    Question:
+    Write a program which accept number from user
+    and count frequency of 4 in it.
+
+    Input : 2395
+    Output : 0
+
+    Input : 9440
+    Output : 2
+*/
+
+#include<stdio.h>
+
+int CountFour(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+
+        if(iDigit == 4)
+        {
+            iCount++;
+        }
+
+        iNo = iNo / 10;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = CountFour(iValue);
+
+    printf("Frequency of 4 is : %d",iRet);
+
+    return 0;
+}
+
+/*
+    Time Complexity : O(n)
+*/
