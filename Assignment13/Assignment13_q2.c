c9b1ffa21f8b0ea31c58e42bc6d23392c12cb916
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-05 10:20:49 +0530

    Add files via upload

---
+++ Assignment13/Assignment13_q2.c
@@ -0,0 +1,61 @@
+/*
+    Question:2
+    Accept N numbers from user and display all such
+    elements which are odd.
+
+    Input : N : 6
+    Elements : 85 66 3 80 93 88
+
+    Output : 85 3 93
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void Display(int Arr[], int iLength)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if((Arr[iCnt] % 2) != 0)
+        {
+            printf("%d\t",Arr[iCnt]);
+        }
+    }
+}
+
+int main()
+{
+    int iSize = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter elements : \n");
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ",iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    Display(p,iSize);
+
+    free(p);
+
+    return 0;
+}
+
+/*
+    Time Complexity : O(n)
+*/
