7161596c7930a99299f273b5044f95a645600dba
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-12 10:21:01 +0530

    Program_Q1 Sum of Digits of a Number.java

---
+++ Assignment17/Sum of Digits of a Number.java
@@ -0,0 +1,27 @@
+class Logic
+{
+    void sumOfDigits(int num)
+    {
+        // Write logic here
+        int iSum = 0;
+        int iDigit = 0;
+
+        while(num > 0)
+        {
+            iDigit = num % 10;    // extract last digit
+            iSum = iSum + iDigit; // add it to sum
+            num = num / 10;       // remove last digit
+        }
+
+        System.out.println("Sum of digits = " + iSum);
+    }
+}
+
+class Program
+{
+    public static void main(String args[])
+    {
+        Logic obj = new Logic();
+        obj.sumOfDigits(1234);
+    }
+} 
