b2c7f25c582c211e6f3ee75952951fe46481029e
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-12 10:53:01 +0530

    Display Grade Based on Marks

---
+++ Assignment19/Assignment19_Q2.java
@@ -0,0 +1,39 @@
+class Logic
+{
+    void displayGrade(int marks)
+    {
+        if(marks >= 90)
+        {
+            System.out.println("Grade : A+");
+        }
+        else if(marks >= 80)
+        {
+            System.out.println("Grade : A");
+        }
+        else if(marks >= 70)
+        {
+            System.out.println("Grade : B");
+        }
+        else if(marks >= 60)
+        {
+            System.out.println("Grade : C");
+        }
+        else if(marks >= 40)
+        {
+            System.out.println("Grade : D");
+        }
+        else
+        {
+            System.out.println("Grade : F  (Fail)");
+        }
+    }
+}
+
+class Program
+{
+    public static void main(String args[])
+    {
+        Logic obj = new Logic();
+        obj.displayGrade(82);
+    }
+}
