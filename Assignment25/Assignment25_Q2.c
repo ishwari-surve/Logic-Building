4f59b85f9d09f3fcaabeb3b9e9c214b02c5fae6c
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-16 01:10:49 +0530

    Alternate alphabet row -number row

---
+++ Assignment25/Assignment25_Q2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    
+    int iR   = 0;
+    int iC   = 0;
+    int iNum = 0;
+
+    for(iR = 1; iR <= iRow; iR++)
+    {
+        if(iR % 2 == 0)         
+        {
+            iNum = 1;
+        }
+        else                     
+        {
+            iNum = 2;
+        }
+
+    for(iC = 1;iC <= iCol; iC++)
+    {
+        printf("%d",iNum);
+        iNum = iNum + 2;
+
+    }
+    printf("\n");
+
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+    printf("Enter number of rows and columns");
+    scanf("%d %d", &iValue1, &iValue2);
+    Pattern(iValue1, iValue2);
+    return 0;
+}
