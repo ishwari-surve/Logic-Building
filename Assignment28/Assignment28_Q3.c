212f212c94d0117849a5d117ab939b3f0c798c7a
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-17 17:58:25 +0530

    Print hollow box pattern with star borde

---
+++ Assignment28/Assignment28_Q3.c
@@ -0,0 +1,45 @@
+// 3. Accept number of rows and number of columns from user and display belowpattern.
+//Input : iRow = 6 iCol = 6
+/*
+Output :
+ * * * * * *
+* * *
+* * *
+* * *
+* * *
+* * * * * *
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    // Logic
+    int i = 0;
+    int j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(j == (iCol - i) + 1|| i == 1 || i == iRow || j == 1 || j == iCol )
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");          
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &iValue1, &iValue2);
+    Pattern(iValue1, iValue2);
+    return 0;
+}
