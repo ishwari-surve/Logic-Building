aad0643332616f99e034892766ada308d6a564fa
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-23 12:22:42 +0530

    Added program to display character sequence using while loop and ASCII character ranges

---
+++ Assignment30/Assignment30_Q3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void Display(char ch)
+{
+    if((ch >= 'A') && (ch <= 'Z'))
+    {
+        while(ch <= 'Z')
+        {
+            printf("%c\t",ch);
+            ch++;
+        }
+    }
+    else if((ch >= 'a') && (ch <= 'z'))
+    {
+        while(ch >= 'a')
+        {
+            printf("%c\t",ch);
+            ch--;
+        }
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    char cValue = '\0';
+
+    printf("Enter the character : ");
+    scanf("%c",&cValue);
+
+    Display(cValue);
+
+    return 0;
+}
