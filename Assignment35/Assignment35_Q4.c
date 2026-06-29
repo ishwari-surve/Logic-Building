0e67946c445870d675f55556f9ba39aad8b9bb14
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-29 12:18:51 +0530

    Add program to check if 7th 8th and 9th bits are all ON using hex mask

---
+++ Assignment35/Assignment35_Q4.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+typedef int BOOL;
+typedef unsigned int UINT;
+
+#define TRUE  1
+#define FALSE 0
+
+BOOL ChkBit(UINT iNo)
+{
+    UINT iMask = 0x000001C0;
+
+    if((iNo & iMask) == iMask)
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int main()
+{
+    UINT iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter number : ");
+    scanf("%u", &iValue);
+
+    bRet = ChkBit(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("7th, 8th and 9th bits are all ON\n");
+    }
+    else
+    {
+        printf("7th, 8th and 9th bits are NOT all ON\n");
+    }
+
+    return 0;
+}
