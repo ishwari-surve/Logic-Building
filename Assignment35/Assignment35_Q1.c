90c8d2f7383ebbbcd324c74a118598ef01774e0f
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-29 11:55:10 +0530

    Add program to check if 15th bit is ON or OFF using hex mask

---
+++ Assignment35/Assignment35_Q1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+typedef int BOOL;
+typedef unsigned int UINT;
+
+#define TRUE  1
+#define FALSE 0
+
+BOOL ChkBit(UINT iNo)
+{
+    UINT iMask = 0x00008000;   
+
+    if((iNo & iMask) == iMask)
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int main()
+{
+    UINT iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter number : ");
+    scanf("%u", &iValue);
+
+    bRet = ChkBit(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("15th bit is ON\n");
+    }
+    else
+    {
+        printf("15th bit is OFF\n");
+    }
+
+    return 0;
+}
