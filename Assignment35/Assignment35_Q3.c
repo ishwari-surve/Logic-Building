7a90ea25d3174f478bf6e22203043d7bbf762e89
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-29 12:14:02 +0530

    Add program to check if 7th 15th 21st and 28th bits are all ON using hex mask

---
+++ Assignment35/Assignment35_Q3.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+typedef int BOOL;
+typedef unsigned int UINT;
+
+#define TRUE  1
+#define FALSE 0
+
+BOOL ChkBit(UINT iNo)
+{
+
+    UINT iMask = 0x08104040;
+
+    if((iNo & iMask) == iMask)
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int main()
+{
+    UINT iValue = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter number : ");
+    scanf("%u", &iValue);
+
+    bRet = ChkBit(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("7th, 15th, 21st and 28th bits are all ON\n");
+    }
+    else
+    {
+        printf("7th, 15th, 21st and 28th bits are OFF\n");
+    }
+
+    return 0;
+}
