4876c9cde819282282280837212aac54778c5525
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-30 14:45:30 +0530

    Added program to set the first four bits of a number using bitwise OR

---
+++ Assignment36/Assignment36_Q5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+UINT OnBit(UINT iNo)
+{
+    UINT iMask = 0x0F;
+
+    return (iNo | iMask);
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%u",&iValue);
+
+    iRet = OnBit(iValue);
+
+    printf("Modified number is %u\n",iRet);
+
+    return 0;
+}
