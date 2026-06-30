5522a447787f5f84f38990eb9af5047821e31a3c
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-06-30 14:34:21 +0530

    Added program to clear the 7th and 10th bits of a number using bitwise AND and combined mask

---
+++ Assignment36/Assignemnt36_Q2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+UINT OffBit(UINT iNo)
+{
+    UINT iMask = 0x240;
+
+    iMask = ~iMask;
+
+    return (iNo & iMask);
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%u",&iValue);
+
+    iRet = OffBit(iValue);
+
+    printf("Modified number is %u\n",iRet);
+
+    return 0;
+}
