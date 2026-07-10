e2111065fa7df9d99cfdfc3815e3318745f6a904
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-10 10:38:15 +0530

    Added FirstOccur function to find the first occurrence position of a given element in a singly linked list

---
+++ Assignment40/Assignment40_Q2.c
@@ -0,0 +1,98 @@
+//Write a program to return the position of the first occurrence of a given element in a Singly Linear Linked List. If the element is not found, return -1.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+   int data;
+   struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int No)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = No;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first->next;
+    }
+
+    printf("NULL\n");
+}
+
+int FirstOccur(PNODE Head, int No)
+{
+    int iPos = 1;
+
+    while(Head != NULL)
+    {
+        if(Head->data == No)
+        {
+            return iPos;
+        }
+
+        Head = Head->next;
+        iPos++;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    PNODE Head = NULL;
+    int iCount = 0, i = 0, iNo = 0;
+    int iRet = 0;
+
+    printf("Enter the number of nodes : ");
+    scanf("%d",&iCount);
+
+    printf("Enter the elements : \n");
+    for(i = 1; i <= iCount; i++)
+    {
+        scanf("%d",&iNo);
+        InsertFirst(&Head,iNo);
+    }
+
+    Display(Head);
+
+    printf("Enter element to search : ");
+    scanf("%d",&iNo);
+
+    iRet = FirstOccur(Head,iNo);
+
+    if(iRet == -1)
+    {
+        printf("Element not found\n");
+    }
+    else
+    {
+        printf("First occurrence is at position : %d\n",iRet);
+    }
+
+    return 0;
+}
