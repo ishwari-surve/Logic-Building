bc19e1fa0959440e4e9ec9e8be3c56d30fc67c23
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-10 10:49:32 +0530

    Added CountLess function to count elements less than a given value in a singly linked list

---
+++ Assignment40/Assignment40_Q5.c
@@ -0,0 +1,89 @@
+//Write a program to count the number of elements less than the given value (X) in a Singly Linear Linked List.
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int No)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = No;
+    newn->next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("| %d |->",first->data);
+        first = first->next;
+    }
+
+    printf("NULL\n");
+}
+
+int CountLess(PNODE Head, int X)
+{
+    int iCount = 0;
+
+    while(Head != NULL)
+    {
+        if(Head->data < X)
+        {
+            iCount++;
+        }
+
+        Head = Head->next;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    PNODE Head = NULL;
+    int iCount = 0, i = 0, iNo = 0;
+    int iRet = 0;
+
+    printf("Enter the number of nodes : ");
+    scanf("%d",&iCount);
+
+    printf("Enter the elements : \n");
+    for(i = 1; i <= iCount; i++)
+    {
+        scanf("%d",&iNo);
+        InsertFirst(&Head,iNo);
+    }
+
+    Display(Head);
+
+    printf("Enter the value : ");
+    scanf("%d",&iNo);
+
+    iRet = CountLess(Head,iNo);
+
+    printf("Number of elements less than %d are : %d\n",iNo,iRet);
+
+    return 0;
+}
