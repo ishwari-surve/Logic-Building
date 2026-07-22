18d846b38b700308c18455972a7df24c1c395981
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-22 12:13:46 +0530

    Add Q4: Generic function to find largest value in array using template

---
+++ Assignment41/Assignment41_Q4.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+T Max(T *arr, int iSize)
+{
+    // Logic
+    T iMax = arr[0];        
+    int i  = 0;
+
+    for(i = 1; i < iSize; i++)
+    {
+        if(arr[i] > iMax)
+        {
+            iMax = arr[i];  
+        }
+    }
+
+    return iMax;
+}
+
+int main()
+{
+    int arr[]   = {10, 20, 30, 40, 50};
+    float brr[] = {10.0, 3.7, 9.8, 8.7};
+
+    int iRet = Max(arr, 5);
+    cout << "Max of int array   : " << iRet << endl;
+
+    float fRet = Max(brr, 4);
+    cout << "Max of float array : " << fRet << endl;
+
+    return 0;
+}
