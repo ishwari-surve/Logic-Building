6fb990c2d3ab18615654d2984cf6a05b922a6010
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-25 21:01:56 +0530

    Added recursive C program to display uppercase alphabets

---
+++ Assignment44/Assignment44_Q4.c
@@ -0,0 +1,26 @@
+// Program 4
+// Input : 6
+// Output : A B C D E F
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    if(iNo >= 1)
+    {
+        Display(iNo - 1);
+        printf("%c\t", 'A' + iNo - 1);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : ");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
