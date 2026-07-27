4b6910f55fa39d2bb01f9596f92e6a67317949f3
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-27 16:11:15 +0530

    Add Q5 - Reverse a number using recursion

---
+++ Assignment46/Assignment46_Q5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int Reverse(int iNo)
+{
+    static int iRev = 0;
+
+    if(iNo != 0)
+    {
+        iRev = (iRev * 10) + (iNo % 10);
+        Reverse(iNo / 10);
+    }
+
+    return iRev;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+
+    printf("Enter number : ");
+    scanf("%d",&iValue);
+
+    iRet = Reverse(iValue);
+
+    printf("Reverse number is : %d\n",iRet);
+
+    return 0;
+}
