ee0d087f0707bc304e46711c9b37bcf0327c9a4a
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-07-30 22:04:09 +0530

    Add Q3: C - Read and Display File Contents

---
+++ Assignment48/Assignment48_Q3.c
@@ -0,0 +1,41 @@
+// QUESTION 3: Accept file name and read all data from file and display contents
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+
+#define BUFFER_SIZE 1024
+
+void DisplayFile(char *Filename)
+{
+    char Buffer[BUFFER_SIZE] = {'\0'};
+    int iRet = 0, fd = 0;
+
+    fd = open(Filename, O_RDONLY);
+
+    if(fd == -1)
+    {
+        return;
+    }
+
+    while((iRet = read(fd, Buffer, sizeof(Buffer))) > 0)
+    {
+        write(STDOUT_FILENO, Buffer, iRet);
+        memset(Buffer, '\0', sizeof(Buffer));
+    }
+
+    close(fd);
+}
+
+int main()
+{
+    char Fname[20] = {'\0'};
+
+    printf("Enter  Filename :\n");
+    scanf("%[^'\n']s", Fname);
+
+    printf("Display all data of file :\n");
+    DisplayFile(Fname);
+
+    return 0;
+}
