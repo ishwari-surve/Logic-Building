fc67d589810e504cebf022fdb98ec3e3489f7ac3
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-08 18:53:41 +0530

    Logic Building - Assignment 45 Q1: File Opening with Existence Check  - Accept file name from user via Scanner - Create File object with given filename - Validate file existence using exists() method - Open file using FileInputStream if exists - Display success/error message - Close file stream properly

---
+++ Assignment49/Assignment49_Q1.java
@@ -0,0 +1,38 @@
+// Question 1: Accept File Name and Open It
+
+import java.io.*;
+import java.util.*;
+
+class Assignment_45_Q1
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        try
+        {
+            System.out.println("Enter file name : ");
+            String strFileName = sc.nextLine();
+            
+    
+            File fobj = new File(strFileName);
+            
+            
+            if(fobj.exists())
+            {
+                
+                FileInputStream fin = new FileInputStream(strFileName);
+                System.out.println("File opened successfully!");
+                fin.close();
+            }
+            else
+            {
+                System.out.println("File does not exist!");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception : "+e);
+        }
+    }
+}
