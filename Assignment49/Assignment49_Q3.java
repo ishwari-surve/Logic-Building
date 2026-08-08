1cd26cfc4ee6b147392ae037ffe9e8b0b9b508c7
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-08 19:09:44 +0530

    Logic Building - Assignment 45 Q3: Append data to existing file

---
+++ Assignment49/Assignment49_Q3.java
@@ -0,0 +1,42 @@
+// Program to accept file name from user and open that file in write mode and write some data at the end of file
+import java.io.*;
+import java.util.*;
+
+class Assignment_45_Q3
+{
+    public static void main(String A[]) throws IOException
+    {
+        String Fname = null;
+        String Data = null;
+        File fobj = null;
+        FileOutputStream foobj = null;
+        
+        Scanner sobj = new Scanner(System.in);
+        
+        System.out.println("Enter file name :");
+        Fname = sobj.nextLine();
+        
+        fobj = new File(Fname);
+        
+        if(fobj.exists())
+        {
+            System.out.println("Enter data to append :");
+            Data = sobj.nextLine();
+            
+            foobj = new FileOutputStream(fobj, true);
+            
+            byte Arr[] = Data.getBytes();
+            
+         
+            foobj.write(Arr);
+            
+            System.out.println("Data appended successfully!");
+            
+            foobj.close();
+        }
+        else
+        {
+            System.out.println("There is no such file");
+        }
+    }
+}
