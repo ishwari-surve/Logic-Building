728da03b1be5848895daf5e0dc28f093f91c1f90
Ishwari Surve <155650956+ishwari-surve@users.noreply.github.com>
2026-08-08 19:07:47 +0530

    Logic Building - Assignment 49 Q2: Read file and display contents

--- Assignment49/Assignment49_Q2.java
+++ Assignment49/Assignment49_Q2.java
@@ -1,50 +1,39 @@
 // Question2 : Read File and Display Contents
+// Program to accept file name from user and open that file and display the contents on screen
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
-class Assignment_45_Q2_Buffer 
+class Assignment_45_Q2
 {
-    public static void main(String args[])
+    public static void main(String A[]) throws IOException
     {
-        Scanner sc = new Scanner(System.in);
+        String Fname = null;
+        File fobj = null;
+        FileInputStream fiobj = null;
         
-        try
+        Scanner sobj = new Scanner(System.in);
+        
+        System.out.println("Enter file name :");
+        Fname = sobj.nextLine();
+        
+        fobj = new File(Fname);
+        
+        if(fobj.exists())
         {
-            System.out.println("Enter file name : ");
-            String strFileName = sc.nextLine();
+            fiobj = new FileInputStream(fobj);
             
-            File fobj = new File(strFileName);
+            byte Arr[] = new byte[1024];
+            int iRet = 0;
             
-            if(fobj.exists())
-            {
-                
-                FileInputStream fin = new FileInputStream(strFileName);
-                
-                
-                byte[] buffer = new byte[1024];  
-                int bytesRead;
-                
-                
-                while((bytesRead = fin.read(buffer)) != -1)
-                {
-                   
-                    for(int i = 0; i < bytesRead; i++)
-                    {
-                        System.out.print((char)buffer[i]);
-                    }
-                }
-                
-                System.out.println("\n\nFile reading completed!");
-                fin.close();
-            }
-            else
-            {
-                System.out.println("File does not exist!");
-            }
+            iRet = fiobj.read(Arr);
+            
+            System.out.println(new String(Arr));
+            
+            fiobj.close();
         }
-        catch(Exception e)
+        else
         {
-            System.out.println("Exception : "+e);
+            System.out.println("There is no such file");
         }
     }
 }
